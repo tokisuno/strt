b1f60d40344035c7c325df269b29a47efe8176e8
tokisuno <lucaswmei@gmail.com>
2024-07-28 19:32:38 -0400

    base startpage ready

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("div.container");
+const list = container.querySelectorAll("ul li");
+
+console.log(list);
+
+// TODO
+// * add emacs-dashboard like bindings to access links
+// * add weather somewhere
+// * spawn list javascript 
+//      - websites stored in csv file
